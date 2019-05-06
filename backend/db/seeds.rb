77d8c02f49e6eaa9b9c4519738799c7eab894463
github username <github email address>
2019-05-06 19:09:04 -0400

    initial HTML

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
+Gender.destroy_all
+Orientation.destroy_all
+Character.destroy_all
+
 Gender.create(name:'male')
 Gender.create(name:'trans-male')
 Gender.create(name:'trans-masculine')
@@ -41,3 +47,40 @@ Orientation.create(name:'Questioning')
 Orientation.create(name:'Asexual')
 Orientation.create(name:'Demisexual')
 Orientation.create(name:'Queer')
+
+
+Character.create(
+    name: Faker::Games::ElderScrolls.unique.name,
+    race: 'Dwarf',
+    character_class: 'Fighter',
+    img: 'https://dungeonsmaster.com/wp-content/uploads/2011/04/dwarf1.jpg',
+    bio: Faker::Books::Lovecraft.paragraph
+)
+Character.create(
+    name: Faker::Games::ElderScrolls.unique.name,
+    race: 'Human',
+    character_class: 'Cleric',
+    img: 'https://vignette.wikia.nocookie.net/dd-the-soul-saga/images/2/2a/Blair_HD.jpg/revision/latest?cb=20160101172747',
+    bio: Faker::Books::Lovecraft.paragraph
+)
+Character.create(
+    name: Faker::Games::ElderScrolls.unique.name,
+    race: 'Elf',
+    character_class: 'Wizard',
+    img: 'https://i.pinimg.com/originals/55/e0/40/55e0406c5885aa522c22d38677cf5056.png',
+    bio: Faker::Books::Lovecraft.paragraph
+)
+Character.create(
+    name: Faker::Games::ElderScrolls.unique.name,
+    race: 'Gnome',
+    character_class: 'Bard',
+    img: 'https://i1.sndcdn.com/avatars-000094814001-y25sqe-t500x500.jpg',
+    bio: Faker::Books::Lovecraft.paragraph
+)
+Character.create(
+    name: Faker::Games::ElderScrolls.unique.name,
+    race: 'Half-Orc',
+    character_class: 'Barbarian',
+    img: 'https://i.pinimg.com/originals/91/f6/7a/91f67a3184174eb4ffda6c381a9991b5.png',
+    bio: Faker::Books::Lovecraft.paragraph
+)
