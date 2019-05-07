53fecb3739f0981621447bf63dbb20787c1ef908
Mouni Mullapudi <mullapudi24@gmail.com>
2019-05-07 09:30:02 -0400

    more tables

---
+++ backend/app/models/quest_event.rb
@@ -0,0 +1,2 @@
+class QuestEvent < ApplicationRecord
+end
---
+++ backend/db/migrate/004_create_date_events.rb
@@ -0,0 +1,12 @@
+class CreateDateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :quests_events do |t|
+      t.string :title
+      t.string :situation
+      t.string :challenge_type
+      t.integer :challenge_rating
+
+      t.timestamps
+    end
+  end
+end
