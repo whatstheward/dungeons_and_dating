a3f3fbcfa3549fed72e895b957ce0dc989741a4f
github username <github email address>
2019-05-07 11:15:51 -0400

    testing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = `http://localhost:3000/`
-const CHAR_GENDERS_URL = `http://localhost:3000/characters/${character.id}/genders`
-const CHAR_ORIENTATIONS_URL = `http://localhost:3000/characters/${character.id}/orientations`
+const CHAR_URL = `http://localhost:3000/characters/`
+
 
 document.addEventListener('DOMContentLoaded', function(){
 console.log('The DOM is loaded')
@@ -17,26 +17,25 @@ function fetchCharacters(){
 }
 
 function fetchCharactersGenders(character){
-    fetch(BASE_URL + )
+    fetch(CHAR_URL + `${character.id}/genders`)
+    .then(res => res.json())
+    .then(genders => renderGenders(genders))
+}
+
+function renderGenders(genders){
+    console.log('charcter genders',genders)
+}
+
+function buildCharacterModal(character){
+    console.log('character', character)
+    fetchCharactersGenders(character)
 }
 
 function renderCharacter(character){
     let div = document.querySelector('#char-columns')
-    
+    buildCharacterModal(character)
     let card = document.createElement('div')
-    card.className = 'card'
-    card.style = "padding: 3em;"
-    
-    
-    let columns = document.createElement('div')
-    columns.className = "columns is-vcentered"
-    
-    let imageDiv = document.createElement('div')
-    imageDiv.className = 'card-content'
-    
-    let figure = document.createElement('figure')
-    figure.className = "media"
-    figure.style = "justify-content: center;"
+    card.className = 'card column is-narrow'
     
     let img = document.createElement('img')
     img.src = character.img
@@ -49,10 +48,6 @@ function renderCharacter(character){
     name.className = "title"
     name.innerText = character.name
 
-    
-    let bio = document.createElement('p')
-    bio.className = "card-content"
-    bio.innerText = character.bio
 
     let btn = document.createElement('button')
     btn.className = "button is-success"
@@ -61,21 +56,14 @@ function renderCharacter(character){
     
     btn.addEventListener('click', showCharacterInfo)
 
-    card.appendChild(columns)
-    figure.appendChild(img)
-    imageDiv.appendChild(figure)
     contentDiv.appendChild(name)
     contentDiv.appendChild(btn)
-    contentDiv.appendChild(bio)
-    card.appendChild(imageDiv)
+    card.appendChild(img)
     card.appendChild(contentDiv)
     div.appendChild(card)
 
 }
 
 function showCharacterInfo(e){
-    let id = e.target.dataset.id
-    fetch(BASE_URL +`characters/${id}`)
-    .then(res => res.json())
-    .then(character => console.log(character.genders))
+    console.log("event target", e.target)
 }
