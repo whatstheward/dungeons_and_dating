eb61762ba1109fbf194cc2c724ee5ea125631a66
github username <github email address>
2019-05-08 09:34:46 -0400

    cleaning house

--- backend/db/old/006_create_character_quests.rb
+++
@@ -1,9 +0,0 @@
-class CreateCharacterQuests < ActiveRecord::Migration[5.2]
-  def change
-    create_table :character_quests do |t|
-      t.belongs_to :character, foreign_key: true
-      t.belongs_to :quest, foreign_key: true
-      t.integer :points
-    end
-  end
-end
