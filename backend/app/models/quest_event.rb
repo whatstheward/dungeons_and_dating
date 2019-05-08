dff9f416d2e4883d6c2fce65fb7a2a43cc656b93
github username <github email address>
2019-05-07 21:24:37 -0400

    don't merge yet, still testing

--- backend/app/models/quest_event.rb
+++ backend/app/models/quest_event.rb
@@ -1,3 +1,5 @@
 class QuestEvent < ApplicationRecord
-  has_many :quests
+  has_many :quests, :foreign_key => 'quest_event1'
+  has_many :quests, :foreign_key => 'quest_event2'
+  has_many :quests, :foreign_key => 'quest_event3'
 end
